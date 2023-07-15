79fc4774b97d78c0e7950afebeca9dbeaab17f51
BROWNPU9 <137032025+BrownLEE99@users.noreply.github.com>
2023-07-15 14:14:40 +0900

    [Silver II] Title: 과자 나눠주기, Time: 268 ms, Memory: 5824 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
@@ -6,6 +6,9 @@
 using namespace std;
 int M,N;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     cin >> M >> N;
     vector<int> snack(N,0);
     int s = 0;
