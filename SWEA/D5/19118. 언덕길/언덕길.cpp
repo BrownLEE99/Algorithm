df7e1f563aa06b874d36c076554484ffdf7827d5
BROWNPU9 <137032025+BrownLEE99@users.noreply.github.com>
2024-02-05 16:03:27 +0900

    [D5] Title: 언덕길, Time: 24 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D5/19118. 언덕길/언덕길.cpp
+++ SWEA/D5/19118. 언덕길/언덕길.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int N;
-int dp[1001];
-int h[1001];
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -13,29 +10,32 @@ int main()
     cout.tie(0);
 
     int test_case;
-    cin >> test_case;
-
-    for(int t = 1; t <= test_case; ++t)
+    int T;
+    cin >> T;
+    for(test_case = 1; test_case <= T; test_case++)
     {
+        int N;
         cin >> N;
-        int height;
-        for(int i = 1; i <= N; ++i)
-        {  
-            cin >> h[i];
+        int L[1001];
+        int dp[1001];
+        for(int i = 1; i <= N; i++)
+        {
+            cin >> L[i];
             dp[i] = 1;
         }
         int max_length = 0;
-        for(int i = 1; i <= N; ++i)
+
+        for(int i = 1; i <= N; i++)
         {
-            for(int j = 1; j <= i; ++j)
+            for(int j = 1; j<= i; j++)
             {
-                if(h[j] < h[i])
+                if(L[j] < L[i])
                 {
-                    dp[i] = max(dp[i], dp[j] + 1);
+                    dp[i] = max(dp[i],dp[j] + 1);
                 }
             }
             max_length = max(max_length,dp[i]);
         }
-        cout << "#" << t << " " << N - max_length << "\n";
+        cout << "#" << test_case << " " << N- max_length << "\n";
     }
 }
